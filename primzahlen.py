5cbda0a0adaf5de9811dbb0f4b8eccd102f73515
HEJE1998 <JeldrikHemme98@web.dem>
2021-11-16 19:15:22 +0100

    Version 1: Programm begonnen

---
+++ primzahlen.py
@@ -0,0 +1,22 @@
+#Jeldrik Hemme
+#ETS 2021
+#16.11.2021
+
+#Hardware: Es wird keine Hardware benötigt
+
+#Version:0.1
+
+#Aufgabe: Ermitteln Sie für einen festgelegten Zahlenbereich
+#die Primzahlen und schreiben Sie diese in eine Liste. Geben Sie die Liste anschließend aus.
+
+erste_zahl = input('Bitte eine Zahl eingeben ')
+zweite_zahl = input('Bitte eine Zahl eingeben ')
+
+erste_zahl = int(erste_zahl)
+zweite_zahl = int(zweite_zahl)
+
+
+print(primzahlen)
+
+
+
